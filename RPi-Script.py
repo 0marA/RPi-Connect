372a4141211801d9596e6ae2cdad49009166d59e
0marA <theengineermachine@gmail.com>
2022-08-01 23:19:08 -0500

    RPi Script and Toggling Tuya Outlets

--- RPi-Script.py
+++ RPi-Script.py
@@ -1,5 +1,9 @@
+import RPi.GPIO as GPIO
 import tinytuya
 from secrets import CHARGING_STATION_DEVICE_ID, CHARGING_STATION_IP, CHARGING_STATION_KEY, CELLO_LAMP_DEVICE_ID, CELLO_LAMP_IP, CELLO_LAMP_KEY, BEDSIDE_LAMPS_DEVICE_ID, BEDSIDE_LAMPS_IP, BEDSIDE_LAMPS_KEY
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 celloLamp = tinytuya.OutletDevice(CELLO_LAMP_DEVICE_ID, CELLO_LAMP_IP, CELLO_LAMP_KEY)
 chargingStationLamp = tinytuya.OutletDevice(CHARGING_STATION_DEVICE_ID, CHARGING_STATION_IP, CHARGING_STATION_KEY)
@@ -9,6 +13,8 @@ celloLamp.set_version(3.3)
 chargingStationLamp.set_version(3.1)
 bedsideLamp.set_version(3.1)
 
-celloLamp.set_status(not celloLamp.status()['dps']['1'])
-chargingStationLamp.set_status(not chargingStationLamp.status()['dps']['1'])
-bedsideLamp.set_status(not bedsideLamp.status()['dps']['1'])
+while True:
+    if GPIO.input(10) == GPIO.HIGH:
+        celloLamp.set_status(not celloLamp.status()['dps']['1'])
+        chargingStationLamp.set_status(not chargingStationLamp.status()['dps']['1'])
+        bedsideLamp.set_status(not bedsideLamp.status()['dps']['1'])
